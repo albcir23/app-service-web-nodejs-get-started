57f213dac25674f477ee794ae5fa8db1ccff3502
albcir23 <albertocirota@gmail.com>
2025-09-10 17:03:13 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
