3944cb99898314a4fb4c49567ba419dc1152b85d
albcir23 <albertocirota@gmail.com>
2025-09-10 17:45:31 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+app.get('/error', (req, res) => { throw new Error('Simulazione 500 per test SRE'); });
+
+
 router.get('/', function(req, res) {
   res.status(200).send('Hello World!');
 });
