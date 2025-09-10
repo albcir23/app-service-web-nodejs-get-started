2144547c5e1f5730ee5ed84704552e5cd54f8de1
albcir23 <albertocirota@gmail.com>
2025-09-10 16:58:14 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,6 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 });
 
-app.get('/error', (req, res) => { throw new Error('Simulazione 500 per test SRE'); });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
