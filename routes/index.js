94e9dc7fca1a692041d2d3dcaa990a751a3c2d9b
albcir23 <albertocirota@gmail.com>
2025-09-11 14:27:02 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hello World!' });
 });
 
 router.get('/error', (req, res) => {
