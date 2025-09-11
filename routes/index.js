0da27b912684a9cac270d235ed0bb80a645c748f
albcir23 <albertocirota@gmail.com>
2025-09-11 13:17:42 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/error', (req, res) => { throw new Error('Simulazione 500 per test SRE'); });
-
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/error', (req, res) => {
+  throw new Error('Simulazione 500 per test SRE');
 });
 
 module.exports = router;
